aec79a1a5a3fe24bba31b7489531fe086f39ba5c
AMAZZAL-AMINE <amazzalhusa@gmail.com>
2021-12-14 23:19:18 +0100

    updated

--- public/js/script.js
+++ public/js/script.js
@@ -4,3 +4,14 @@ document.querySelector(".navshowresposivelist").addEventListener("click", () => 
 document.querySelector(".closenavbar").addEventListener("click", () => {
     document.querySelector(".navList").style.left = "1000px";
 });
+
+//lis formation
+// document.querySelector(".listsgowingwhenehide").addEventListener("click", () => {
+//     document.querySelector(".allformationlefftt").style.display = "block";
+//     document.querySelector(".overlayhelolist").style.display = "block";
+// });
+
+// document.querySelector(".overlayhelolist").addEventListener("click", () => {
+//     document.querySelector(".allformationlefftt").style.display = "none";
+//     document.querySelector(".overlayhelolist").style.display = "none";
+// })
--- resources/views/pages/formations.blade.php
+++ resources/views/pages/formations.blade.php
@@ -15,12 +15,26 @@
                <img class="carousel-cell" src="/sliders/4.jpg" alt="">
                <img class="carousel-cell" src="/sliders/5.jpg" alt="">
         </div>
+       
 
         <div class="formationinlistwithdetails">
+            
+            <div class="fomationdetailssss">
+                <h1>Hello This Is Formation One</h1>
+                <p>
+                    molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
+                    numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
+                    optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis
+                    obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam
+                    nihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit,
+                    tenetur error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit,
+                </p>
+            </div>
             <div class="allformationlefftt">
                 <ul>
                     <div class="formationtitleee">
                          <b>Toutes les  Formations</b>
+                          
                     </div>
                     <a href="">
                         <div>Formation Num One</div>
@@ -51,9 +65,6 @@
                         <div><i class="bi bi-arrow-right"></i></div>
                     </a>
                 </ul>
-            </div>
-            <div class="fomationdetailssss">
-                
             </div>
         </div>
     </div>
