d24cd59a12488287a17e1f624a7ea1140823f989
AMAZZAL-AMINE <amazzalhusa@gmail.com>
2021-12-15 12:13:35 +0100

    updated blogs page

--- resources/views/pages/blog.blade.php
+++ resources/views/pages/blog.blade.php
@@ -29,5 +29,113 @@
                 </div>
             </div>
         </div>
+        <hr>
+        <div class="allcartdsblog">
+            <a href="">
+                <div class="cardblog">
+                    <div class="blogcardimg">
+                        <img src="/images/cardimg.jpg" alt="" />
+                    </div>
+                    <div class="blogcardbody">
+                        <div class="cradblogat"><span><i class="bi bi-clock-fill"></i> Mars 01, 2021</span></div>
+                        <div class="cradblogtitle">
+                            <b>Lorem ipsum dolor sit amet, consectetur</b>
+                        </div>
+                        <div class="cardblogdesc">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                        </div>
+                    </div>
+                </div>
+            </a>
+
+            <a href="">
+                <div class="cardblog">
+                    <div class="blogcardimg">
+                        <img src="/images/cardimg.jpg" alt="" />
+                    </div>
+                    <div class="blogcardbody">
+                        <div class="cradblogat"><span><i class="bi bi-clock-fill"></i> Mars 01, 2021</span></div>
+                        <div class="cradblogtitle">
+                            <b>Lorem ipsum dolor sit amet, consectetur</b>
+                        </div>
+                        <div class="cardblogdesc">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                        </div>
+                    </div>
+                </div>
+            </a>
+
+
+            <a href="">
+                <div class="cardblog">
+                    <div class="blogcardimg">
+                        <img src="/images/cardimg.jpg" alt="" />
+                    </div>
+                    <div class="blogcardbody">
+                        <div class="cradblogat"><span><i class="bi bi-clock-fill"></i> Mars 01, 2021</span></div>
+                        <div class="cradblogtitle">
+                            <b>Lorem ipsum dolor sit amet, consectetur</b>
+                        </div>
+                        <div class="cardblogdesc">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                        </div>
+                    </div>
+                </div>
+            </a>
+
+
+            <a href="">
+                <div class="cardblog">
+                    <div class="blogcardimg">
+                        <img src="/images/cardimg.jpg" alt="" />
+                    </div>
+                    <div class="blogcardbody">
+                        <div class="cradblogat"><span><i class="bi bi-clock-fill"></i> Mars 01, 2021</span></div>
+                        <div class="cradblogtitle">
+                            <b>Lorem ipsum dolor sit amet, consectetur</b>
+                        </div>
+                        <div class="cardblogdesc">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                        </div>
+                    </div>
+                </div>
+            </a>
+
+
+            <a href="">
+                <div class="cardblog">
+                    <div class="blogcardimg">
+                        <img src="/images/cardimg.jpg" alt="" />
+                    </div>
+                    <div class="blogcardbody">
+                        <div class="cradblogat"><span><i class="bi bi-clock-fill"></i> Mars 01, 2021</span></div>
+                        <div class="cradblogtitle">
+                            <b>Lorem ipsum dolor sit amet, consectetur</b>
+                        </div>
+                        <div class="cardblogdesc">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                        </div>
+                    </div>
+                </div>
+            </a>
+
+
+            <a href="">
+                <div class="cardblog">
+                    <div class="blogcardimg">
+                        <img src="/images/cardimg.jpg" alt="" />
+                    </div>
+                    <div class="blogcardbody">
+                        <div class="cradblogat"><span><i class="bi bi-clock-fill"></i> Mars 01, 2021</span></div>
+                        <div class="cradblogtitle">
+                            <b>Lorem ipsum dolor sit amet, consectetur</b>
+                        </div>
+                        <div class="cardblogdesc">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                        </div>
+                    </div>
+                </div>
+            </a>
+        </div>
     </div>
 @endsection
