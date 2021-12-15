70e0886677b52f41dce72452a120fee47b4732e1
AMAZZAL-AMINE <amazzalhusa@gmail.com>
2021-12-15 11:09:28 +0100

    updated

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -38,5 +38,29 @@
                 <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3441.1592181987435!2d-9.547844805701438!3d30.403224236160835!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x4eb13d16ba344019!2sInstitut%20IMA!5e0!3m2!1sar!2sma!4v1639559633612!5m2!1sar!2sma" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
             </div>
         </div>
+
+        <div class="contactinfo">
+                <div class="contactdatas">
+                    <div class="emailcontac">
+                        <b><i class="bi bi-envelope"></i> institutimastf@gmail.com</b>
+                    </div>
+                    <div class="numecontact">
+                        <b><i class="bi bi-telephone"></i> 0626-400022</b>
+                    </div>
+                    <div class="numecontact">
+                        <b><i class="bi bi-telephone"></i> 05282-39873</b>
+                    </div>
+                </div>
+                <div class="contactopeningatto">
+                    <h2>Horaires d'ouverture</h2>
+                    <div><p>lun.:	08:30 - 21:00</p></div>
+                    <div><p>mar.:	08:30 - 21:00</p></div>
+                    <div><p>mer.:	08:30 - 21:00</p></div>
+                    <div><p>jeu.:	08:30 - 21:00</p></div>
+                    <div><p>ven.:	08:30 - 21:00</p></div>
+                    <div><p>sam.:	08:30 - 21:00</p></div>
+                    <div><p>dim.:	08:30 - 21:00</p></div>
+                </div>
+        </div>
     </div>
 @endsection
