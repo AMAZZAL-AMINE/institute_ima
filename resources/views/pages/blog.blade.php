8b1f5452f671ced4072bbc32c84bf7167faddf10
AMAZZAL-AMINE <amazzalhusa@gmail.com>
2021-12-15 11:51:04 +0100

    updated

--- resources/views/pages/blog.blade.php
+++ resources/views/pages/blog.blade.php
@@ -8,6 +8,26 @@
 </style>
 
     <div class="container">
-        <h1>Blog Pages</h1>
+        <div class="blogtitletop">
+            <h1>Le Blog</h1>
+        </div>
+
+        <div class="blogpaneltop">
+            <div class="blogimg">
+                <img src="/images/blog.svg" alt="">
+            </div>
+            <div class="blogtexxxx">
+                <div class="bogpaneltitle">
+                    <h2>Regardez Nos Nouvelles Et Quoi De Neuf</h2>
+                </div>
+                <div class="blogpaneldesc">
+                    <p>
+                        Ici dans cette Page, vous trouverez toutes les nouveautés 
+                        et toutes les nouvelles formations ajoutées récemment
+                        à l'Institut IMA
+                    </p>
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
