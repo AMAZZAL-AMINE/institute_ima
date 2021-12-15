dd911d60dbb3f03ba3ee0d0a636976b03237b3d1
AMAZZAL-AMINE <amazzalhusa@gmail.com>
2021-12-15 17:17:17 +0100

    updated

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,6 +1,11 @@
 @extends('layouts.app')
-
+@section('isactiveseven', 'active')
 @section('content')
+<style>
+    .ima_navbar .navList .active {
+        color: orange;
+    }
+</style>
 <div class="container">
     <div class="registeruseraccont">
         <div class="registerleftimg">
@@ -8,6 +13,39 @@
         </div>
         <div class="registerformssss">
             <form method="POST" action="{{ route('register') }}">
+                @csrf
+                <div class="useregisinp">
+                    @error('name')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                    <input id="name" type="text" class="@error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" placeholder="Username" autofocus>
+                </div>
+                <div class="useregisinp">
+                    @error('email')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                    <input id="email" type="email" class="@error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" placeholder="Email">
+                </div>
+                <div class="useregisinp">
+                    @error('password')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                    <input id="password" type="password" class="@error('password') is-invalid @enderror" name="password" required autocomplete="new-password" placeholder="Mot De Pass">
+                </div>
+                <div class="useregisinp">
+                    <input id="password-confirm" type="password" name="password_confirmation" required autocomplete="new-password" placeholder="Confirm Mot De Pass">
+                </div>
+                <button type="submit" class="registerbtntnt">
+                    {{ __('Register') }}
+                </button>
+                <span>Avez-vous déjà un compte ?  <a href="{{ route("login") }}"> S'identifier</a></span>
+            </form>
         </div>
     </div>
 </div>
