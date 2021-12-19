4b04b6f2945b64c532d1dd973bc74255a2648b45
AMAZZAL-AMINE <amazzalhusa@gmail.com>
2021-12-19 13:29:19 +0100

    update dtable reauest users

--- resources/views/admin/requestusers.blade.php
+++ resources/views/admin/requestusers.blade.php
@@ -4,28 +4,24 @@
     <div class="container">
 
             <!-- Page Heading -->
-            <h1 class="h3 mb-2 text-gray-800">Users Request</h1>
-            <p class="mb-4">This Tables Showing All User Who Send Data From Website
+
                
 
             <!-- DataTales Example -->
             <div class="card shadow mb-4">
                 <div class="card-header py-3">
-                    <h6 class="m-0 font-weight-bold text-primary">DataTables Example</h6>
+                    <h6 class="m-0 font-weight-bold text-primary">Les demandes des étudiants</h6>
                 </div>
                 <div class="card-body">
                     <div class="table-responsive">
                         <table class="table table-bordered  table-hover" id="dataTable" width="100%" cellspacing="0">
                             <thead>
                                 <tr>
-                                    <th>Student Id</th>
-                                    <th>Image</th>
+                                    <th>Id</th>
                                     <th>Nom</th>
                                     <th>Prénom</th>
                                     <th>Cin</th>
                                     <th>Phone</th>
-                                    <th>D.Naissance</th>
-                                    <th>Schole</th>
                                     <th>Formation</th>
                                     <th>D.D’inscription</th>
                                     <th>Accept</th>
@@ -35,14 +31,11 @@
                             </thead>
                             <tfoot>
                                 <tr>
-                                    <th>Student Id</th>
-                                    <th>Image</th>
+                                    <th> Id</th>
                                     <th>Nom</th>
                                     <th>Prénom</th>
                                     <th>Cin</th>
                                     <th>Phone</th>
-                                    <th>D.Naissance</th>
-                                    <th>Schole</th>
                                     <th>Formation</th>
                                     <th>D.D’inscription</th>
                                     <th>Accept</th>
@@ -55,13 +48,10 @@
                                 @foreach ($students as $student)
                                 <tr>
                                     <td>{{ $student->id }}</td>
-                                    <td><img style="width: 100%" src="{{ asset("/storage/".$student->image ) }}" alt=""></td>
                                     <td>{{ $student->fname }}</td>
                                     <td>{{ $student->lname }}</td>
                                     <td>{{ $student->cin }}</td>
                                     <td>{{ $student->phone }}</td>
-                                    <td>{{ $student->berthday }}</td>
-                                    <td>{{ $student->nvschole }}</td>
                                     <td>{{ $student->formations->name ?? 'none' }}</td>
                                     <td>{{ $student->created_at->format('Y-m-d') }}</td>
                                     <td><a class="btn btn-success" href="{{ route("admin.acceptstudent",$student->cin) }}"><i class="fa fa-check" aria-hidden="true"></i></a></td>
