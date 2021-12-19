d155bd78831a3bac1f8581259f1c5c439b1841a4
AMAZZAL-AMINE <amazzalhusa@gmail.com>
2021-12-19 16:05:01 +0100

    updated

--- resources/views/admin/pages/acceptstudent.blade.php
+++ resources/views/admin/pages/acceptstudent.blade.php
@@ -1,9 +1,64 @@
 @extends('layouts.admin')
 
 @section("content")
-    <div class="container">
+    <div class="container ">
           @foreach ($student as $stud)
-              {{ $stud->fname }}  {{ $stud->lname }}
+                <div class="card">
+                        <div class="card-header">
+                            Informations sur cet étudiant
+                        </div>
+                    <div class="card-body">
+                        <div class="container-fluid input-group d-flex align-items-center mb-3">
+                            <div> <span>Nom & Prénom : </span> </div> 
+                            <div><b class="text-primary">&nbsp;&nbsp;&nbsp; {{ $stud->fname }} {{ $stud->lname }} </b></div>
+                        </div>
+                        <hr>
+                        <div class="container-fluid input-group d-flex align-items-center mb-3">
+                            <div> <span>Carte/N : </span> </div> 
+                            <div><b class="text-primary">&nbsp;&nbsp;&nbsp; {{ $stud->cin }} </b></div>
+                        </div>
+                        <hr>
+                        <div class="container-fluid input-group d-flex align-items-center mb-3">
+                            <div> <span>Phone : </span> </div> 
+                            <div><b class="text-primary">&nbsp;&nbsp;&nbsp; {{ $stud->phone }} </b></div>
+                        </div>
+                        <hr>
+                        <div class="container-fluid input-group d-flex align-items-center mb-3">
+                            <div> <span>D.Inscription : </span> </div> 
+                            <div><b class="text-primary">&nbsp;&nbsp;&nbsp; {{ $stud->created_at->format('Y-m-d') }} </b></div>
+                        </div>
+                        <hr>
+                        <div class="container-fluid input-group d-flex align-items-center mb-3">
+                            <div> <span>D.De Naissance : </span> </div> 
+                            <div><b class="text-primary">&nbsp;&nbsp;&nbsp; {{ $stud->berthday}} </b></div>
+                        </div>
+                        <hr>
+                        <div class="container-fluid input-group d-flex align-items-center mb-3">
+                            <div> <span>Formation : </span> </div> 
+                            <div><b class="text-primary">&nbsp;&nbsp;&nbsp; {{ $stud->formations->name }} </b></div>
+                        </div>
+                        <hr>
+                        <div class="container-fluid  mb-3">
+                                <form action="" method="post">
+                                    @csrf
+                                    @method("PUT")
+                                    <div class="form-group input-group d-flex align-items-center">
+                                        <div> <span>Prix ​​d'inscription :</span> </div>
+                                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                                       <input class="form-control" type="number"  name="prix_iscription" placeholder="Prix (DH)">
+                                    </div>
+
+                                    <div class="form-group input-group d-flex align-items-center">
+                                        <div> <span>Prix  le premier mois :</span> </div>
+                                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                                       <input class="form-control" type="number"  name="prix_iscription" placeholder="Prix (DH)">
+                                    </div>
+                                    <button type="submit" class="btn btn-primary">Save And Next To Page Print</button>
+                                </form>
+                        </div>
+
+                    </div>
+                </div>
           @endforeach
     </div>
 @endsection
