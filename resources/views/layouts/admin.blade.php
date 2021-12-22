439379d9dfa9afde7e26ad195bcfe1012f86b8e8
AMAZZAL-AMINE <amazzalhusa@gmail.com>
2021-12-22 18:34:43 +0100

    uodated

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -81,12 +81,11 @@
       <li class="nav-item">
         <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities" aria-expanded="true" aria-controls="collapseUtilities">
           <i class="fas fa-fw fa-wrench"></i>
-          <span>Orders </span>
+          <span>Manage Payments </span>
         </a>
         <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities" data-parent="#accordionSidebar">
           <div class="bg-white py-2 collapse-inner rounded">
-            <h6 class="collapse-header">Custom Orders:</h6>
-            <a class="collapse-item" href="#">All Orders</a>
+            <h6 class="collapse-header">Custom Payments:</h6>
             <a class="collapse-item" href="#">All Payments</a>
           </div>
         </div>
