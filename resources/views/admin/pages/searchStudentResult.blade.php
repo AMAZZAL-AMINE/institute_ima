d3cb0a8cfcad37f6dc6f32fcf688a37d2c0e5465
AMAZZAL-AMINE <amazzalhusa@gmail.com>
2021-12-24 17:29:43 +0100

    uodated

--- resources/views/admin/pages/searchStudentResult.blade.php
+++ resources/views/admin/pages/searchStudentResult.blade.php
@@ -5,12 +5,15 @@
         <h1>Search Of Student Result </h1>
         <div class="contentofresultstudent">
             <div class="cartcontentofresult">
-                <div class="img"><img src="" alt=""></div>
+                <div class="img"><img src="/images/logo.png" alt=""></div>
                 <div class="cartcontentbody">
-                    <div><h1></h1></div>
+                    <div><h1>Amine Amazzal</h1></div>
                     <div class="cartbodyformandcin">
-                        <div><b></b></div>
-                        <div><b></b></div>
+                        <div><b>Tel: 0691987003</b></div>
+                        <div><b>Cin: JC54548</b></div>
+                    </div>
+                    <div class="buttontoselect -100">
+                        <a  href="">Select </a>
                     </div>
                 </div>
             </div>
