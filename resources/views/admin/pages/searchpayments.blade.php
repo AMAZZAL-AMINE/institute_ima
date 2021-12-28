a93e54794e250ced4aa177186ac767ee9dd15e4e
AMAZZAL-AMINE <amazzalhusa@gmail.com>
2021-12-28 16:20:22 +0100

    updated

--- resources/views/admin/pages/searchpayments.blade.php
+++ resources/views/admin/pages/searchpayments.blade.php
@@ -3,5 +3,25 @@
 @section("content")
     <div class="container">
         <h1>This is Page Of Search Students</h1>
+
+        <div class="searchfrmationusingmonth">
+            <form action="" method="get">
+                <div class="allinputs">
+                    <div class="selectformation">
+                        <span>Select Le Formation</span>
+                        <select name="formation" >
+                            <option value="">Select Formation</option>
+                        </select>
+                    </div>
+                    <div class="selectmonth">
+                        <span>Select Le Mois</span>
+                        <select name="month" >
+                            <option value="">Select Le Mois</option>
+                        </select>
+                    </div>
+                </div>
+                <button type="submit"><i class="fa fa-search"></i> Search</button>
+            </form>
+        </div>
     </div>
 @endsection
