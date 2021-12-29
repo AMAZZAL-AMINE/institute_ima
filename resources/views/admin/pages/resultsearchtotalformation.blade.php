013a99deb3eb710a7267cce0f77b7b175181ed29
AMAZZAL-AMINE <amazzalhusa@gmail.com>
2021-12-29 16:39:23 +0100

    updated

--- resources/views/admin/pages/resultsearchtotalformation.blade.php
+++ resources/views/admin/pages/resultsearchtotalformation.blade.php
@@ -3,6 +3,10 @@
 @section("content")
     <div class="container">
 
+      <div  style="text-align: center; margin-bottom: 50px;">
+        <b >Les Statistiques En <span class="text-danger">{{ $formation }}</span> </b>
+        <hr>
+      </div>
 
         <div class="card shadow mb-4">
               
@@ -15,8 +19,9 @@
                         <thead >
                             <tr>
                                 <th scope="col">Cin</th>
+                                <th>Nom</th>
                                 <th scope="col">Mois</th>
-                                <th scope="col">Formation</th>
+                                {{-- <th scope="col">Formation</th> --}}
                                 <th scope="col">Prix</th>
                                 <th scope="col">Date</th>
                             </tr>
@@ -24,10 +29,10 @@
                         </thead>
                         <tfoot >
                             <tr>
-                            <th class="text-primary">Id</th>
                             <th scope="col">Cin</th>
+                            <th>Nom</th>
                             <th scope="col">Mois</th>
-                            <th scope="col">Formation</th>
+                            {{-- <th scope="col">Formation</th> --}}
                             <th scope="col">Prix</th>
                             <th scope="col">Date</th>
                          </tr>
@@ -37,9 +42,10 @@
                         <tbody>
                             @foreach ($payedmonths as $payment)
                             <tr>
-                                <th>{{ $payment->student_cin }}</th>
+                                <td>{{ $payment->student_cin }}</td>
+                                <td>{{ $payment->students_name }}</td>
                                 <td >{{ $payment->name }}</td>
-                                <td>{{ $payment->formation }}</td>
+                                {{-- <td>{{ $payment->formation }}</td> --}}
                                 <td>{{ $payment->prix }} DH</td>
                                 <td>{{ $payment->created_at->format('Y-m') }}</td>
                                 @if($payment->name === "Premier Mois")
@@ -51,21 +57,16 @@
                     </table>
                 </div>
             </div>
+            <div class="card-footer text-muted text-center">
+                <b>Totale : </b> <b> {{ $payedmonths->sum('prix');  }} DH</b>
+            </div>
         </div>
 
 
-
-         {{-- {{ $payedmonths }} --}}
+ 
             
                 <hr>
     
-
-
-
-
-
-
-
 
                 <div class="card shadow mb-4">
               
@@ -79,7 +80,7 @@
                                     <tr>
                                         <th scope="col">Cin</th>
                                         <th scope="col">Mois</th>
-                                        <th scope="col">Formation</th>
+                                        {{-- <th scope="col">Formation</th> --}}
                                         <th scope="col">Prix</th>
                                         <th scope="col">Date</th>
                                     </tr>
@@ -89,7 +90,7 @@
                                     <tr>
                                         <th scope="col">Cin</th>
                                         <th scope="col">Mois</th>
-                                        <th scope="col">Formation</th>
+                                        {{-- <th scope="col">Formation</th> --}}
                                         <th scope="col">Prix</th>
                                         <th scope="col">Date</th>
                                     </tr>
@@ -101,7 +102,7 @@
                                         <tr>
                                             <th scope="row">{{ $std->student_cin }}</th>
                                             <td >{{ $std->name }}</td>
-                                            <td>{{ $std->formation }}</td>
+                                            {{-- <td>{{ $std->formation }}</td> --}}
                                             <td>{{ $std->prix }} DH</td>
                                             <td>{{ $std->created_at->format('Y-m') }}</td>
                                         </tr> 
@@ -115,39 +116,6 @@
                         <b>Totale : </b> <b> {{ $newstudents->sum('prix');  }} DH</b>
                       </div>
                 </div>
-
-
-
-
-        <h4 style="margin-top: 150px;" class="  text-primary">Nombre Total de nouveaux étudiants ce mois-ci <span class="text-danger">{{ $month }}/{{ $year }}</span> </h5> 
-            <table class="table mt-5 table-responsive">
-                <thead>
-                    <tr>
-                    <th scope="col">Cin</th>
-                    <th scope="col">Mois</th>
-                    <th scope="col">Formation</th>
-                    <th scope="col">Prix</th>
-                    <th scope="col">Date</th>
-                    </tr>
-                </thead>
-                <tbody>
-                @foreach ($newstudents as $std)
-                    <tr>
-                        <th scope="row">{{ $std->student_cin }}</th>
-                        <td >{{ $std->name }}</td>
-                        <td>{{ $std->formation }}</td>
-                        <td>{{ $std->prix }} DH</td>
-                        <td>{{ $std->created_at->format('Y-m') }}</td>
-                    </tr> 
-                @endforeach
-                <tr class="bg-primary text-white">
-                    <th  scope="row">Total</th>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td>{{ $newstudents->sum('prix');  }} DH</td>
-                </tr>
-                </tbody>
 
     </div>
 @endsection
