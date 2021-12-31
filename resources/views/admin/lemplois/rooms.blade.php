88a59f732e9e439989e12822dc7774256145a0ee
AMAZZAL-AMINE <amazzalhusa@gmail.com>
2021-12-31 17:36:33 +0100

    updated

--- resources/views/admin/lemplois/rooms.blade.php
+++ resources/views/admin/lemplois/rooms.blade.php
@@ -2,18 +2,17 @@
 
 @section("content")
     <div class="container">
-        <table class="table-bordered">
+        <table  class="table-bordered  text-center bg-white tableroomss">
             <tr>
-                <td></td>
-                <td></td>
-                <td>8-9</td>
+                <td colspan="2"></td>  
+                <td >8-9</td>
                 <td>9-10</td>
                 <td>10-11</td>
                 <td>11-12</td>
                 <td>12-1</td>
                 <td>1-2</td>
                 <td>2-3</td>
-                <td>3-8</td>
+                <td>3-4</td>
                 <td>4-5</td>
                 <td>5-6</td>
                 <td>6-7</td>
@@ -23,30 +22,30 @@
                 <td>10-11</td>
             </tr>
             <tr>
-                <th rowspan="5">
-                    Ima
+                <th rowspan="5" style="transform: rotate(-90deg)">
+                    IMA
                 </th>
             </tr>
-            <tr>               
+            <tr style="height: 100px;">               
              <th>S1</th>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
+                <td class="p-3"></td>
+                <td class="p-3"></td>
+                <td class="p-3"></td>
+                <td class="p-3"></td>
+                <td class="p-3"></td>
+                <td class="p-3"></td>
+                <td class="p-3"></td>
+                <td class="p-3"></td>
+                <td class="p-3"></td>
+                <td class="p-3"></td>
+                <td class="p-3"></td>
+                <td class="p-3"></td>
+                <td class="p-3"></td>
+                <td class="p-3"></td>
+                <td class="p-3"></td>
             </tr>
-            <tr>               
-                <th>S1</th>
+            <tr style="height: 100px;">               
+                <th>S2</th>
                    <td></td>
                    <td></td>
                    <td></td>
@@ -64,8 +63,8 @@
                    <td></td>
                </tr>
 
-               <tr>               
-                <th>S1</th>
+               <tr style="height: 100px;">               
+                <th>S3</th>
                    <td></td>
                    <td></td>
                    <td></td>
@@ -82,8 +81,8 @@
                    <td></td>
                    <td></td>
                </tr>
-               <tr>               
-                <th>S1</th>
+               <tr style="height: 100px;">               
+                <th>S4</th>
                    <td></td>
                    <td></td>
                    <td></td>
