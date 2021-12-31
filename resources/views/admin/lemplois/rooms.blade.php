c4bc52319490fa785a28729db2956fb4ca2d3df9
AMAZZAL-AMINE <amazzalhusa@gmail.com>
2021-12-31 18:27:15 +0100

    uodated

--- resources/views/admin/lemplois/rooms.blade.php
+++ resources/views/admin/lemplois/rooms.blade.php
@@ -105,5 +105,203 @@
                 
             </table>
         </div>
+
+<hr>
+
+        {{-- institute iphec manageents Rooms --}}
+
+        <div class="tableiphec">
+            <h1 class="text-center">Gestion Des Salles en l`instotute Iphec</h1>
+            <table  class="table-bordered  text-center bg-white tableroomss">
+                <tr>
+                    <td colspan="2"></td>  
+                    <td >8-9</td>
+                    <td>9-10</td>
+                    <td>10-11</td>
+                    <td>11-12</td>
+                    <td>12-1</td>
+                    <td>1-2</td>
+                    <td>2-3</td>
+                    <td>3-4</td>
+                    <td>4-5</td>
+                    <td>5-6</td>
+                    <td>6-7</td>
+                    <td>7-8</td>
+                    <td>8-9</td>
+                    <td>9-10</td>
+                    <td>10-11</td>
+                </tr>
+                <tr style="background-color: blue; color: white;">
+                    <th rowspan="10" style="transform: rotate(-90deg); " >
+                        IPHEC
+                    </th>
+                </tr>
+                <tr class="tirashrashjdfg">               
+                 <th style="color: blue;">R1</th>
+                    <td ></td>
+                    <td ></td>
+                    <td ></td>
+                    <td ></td>
+                    <td ></td>
+                    <td >INFO</td>
+                    <td ></td>
+                    <td ></td>
+                    <td ></td>
+                    <td ></td>
+                    <td ></td>
+                    <td ></td>
+                    <td ></td>
+                    <td ></td>
+                    <td ></td>
+                </tr>
+                <tr class="tirashrashjdfg">               
+                    <th style="color: blue;">R2</th>
+                       <td></td>
+                       <td></td>
+                       <td></td>
+                       <td>ENG</td>
+                       <td></td>
+                       <td></td>
+                       <td></td>
+                       <td>AR</td>
+                       <td></td>
+                       <td></td>
+                       <td></td>
+                       <td></td>
+                       <td></td>
+                       <td></td>
+                       <td></td>
+                   </tr>
+    
+                   <tr class="tirashrashjdfg">               
+                    <th style="color: blue;">R3</th>
+                       <td></td>
+                       <td></td>
+                       <td></td>
+                       <td></td>
+                       <td>FR</td>
+                       <td></td>
+                       <td></td>
+                       <td></td>
+                       <td></td>
+                       <td></td>
+                       <td></td>
+                       <td></td>
+                       <td></td>
+                       <td></td>
+                       <td></td>
+                   </tr>
+                   <tr class="tirashrashjdfg">               
+                    <th style="color: blue;">R4</th>
+                       <td></td>
+                       <td></td>
+                       <td>ENG</td>
+                       <td></td>
+                       <td></td>
+                       <td></td>
+                       <td></td>
+                       <td></td>
+                       <td></td>
+                       <td></td>
+                       <td></td>
+                       <td></td>
+                       <td></td>
+                       <td></td>
+                       <td></td>
+                   </tr>
+                   <tr class="tirashrashjdfg">               
+                    <th style="color: blue;">R4</th>
+                       <td></td>
+                       <td></td>
+                       <td>ENG</td>
+                       <td></td>
+                       <td></td>
+                       <td></td>
+                       <td></td>
+                       <td></td>
+                       <td></td>
+                       <td></td>
+                       <td></td>
+                       <td></td>
+                       <td></td>
+                       <td></td>
+                       <td></td>
+                   </tr>
+                   <tr class="tirashrashjdfg">               
+                    <th style="color: blue;">R4</th>
+                       <td></td>
+                       <td></td>
+                       <td>ENG</td>
+                       <td></td>
+                       <td></td>
+                       <td></td>
+                       <td></td>
+                       <td></td>
+                       <td></td>
+                       <td></td>
+                       <td></td>
+                       <td></td>
+                       <td></td>
+                       <td></td>
+                       <td></td>
+                   </tr>
+                   <tr class="tirashrashjdfg">               
+                    <th style="color: blue;">R4</th>
+                       <td></td>
+                       <td></td>
+                       <td>ENG</td>
+                       <td></td>
+                       <td></td>
+                       <td></td>
+                       <td></td>
+                       <td></td>
+                       <td></td>
+                       <td></td>
+                       <td></td>
+                       <td></td>
+                       <td></td>
+                       <td></td>
+                       <td></td>
+                   </tr>
+                   <tr class="tirashrashjdfg">               
+                    <th style="color: blue;">R4</th>
+                       <td></td>
+                       <td></td>
+                       <td>ENG</td>
+                       <td></td>
+                       <td></td>
+                       <td></td>
+                       <td></td>
+                       <td></td>
+                       <td></td>
+                       <td></td>
+                       <td></td>
+                       <td></td>
+                       <td></td>
+                       <td></td>
+                       <td></td>
+                   </tr>
+                   <tr class="tirashrashjdfg">               
+                    <th style="color: blue;">R4</th>
+                       <td></td>
+                       <td></td>
+                       <td>ENG</td>
+                       <td></td>
+                       <td></td>
+                       <td></td>
+                       <td></td>
+                       <td></td>
+                       <td></td>
+                       <td></td>
+                       <td></td>
+                       <td></td>
+                       <td></td>
+                       <td></td>
+                       <td></td>
+                   </tr>
+                   
+                
+            </table>
+        </div>
     </div>
 @endsection
