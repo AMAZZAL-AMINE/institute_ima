85a03b74c2234e40bf39fe975cb3213649fb6bc1
AMAZZAL-AMINE <amazzalhusa@gmail.com>
2021-12-31 18:15:16 +0100

    uodated

--- resources/views/admin/lemplois/rooms.blade.php
+++ resources/views/admin/lemplois/rooms.blade.php
@@ -1,105 +1,107 @@
 @extends("layouts.admin")
 
 @section("content")
-    <div class="container">
-        <table  class="table-bordered  text-center bg-white tableroomss">
-            <tr>
-                <td colspan="2"></td>  
-                <td >8-9</td>
-                <td>9-10</td>
-                <td>10-11</td>
-                <td>11-12</td>
-                <td>12-1</td>
-                <td>1-2</td>
-                <td>2-3</td>
-                <td>3-4</td>
-                <td>4-5</td>
-                <td>5-6</td>
-                <td>6-7</td>
-                <td>7-8</td>
-                <td>8-9</td>
-                <td>9-10</td>
-                <td>10-11</td>
-            </tr>
-            <tr>
-                <th rowspan="5" style="transform: rotate(-90deg)">
-                    IMA
-                </th>
-            </tr>
-            <tr class="tirashrashjdfg">               
-             <th>S1</th>
-                <td ></td>
-                <td ></td>
-                <td ></td>
-                <td ></td>
-                <td ></td>
-                <td ></td>
-                <td ></td>
-                <td ></td>
-                <td ></td>
-                <td ></td>
-                <td ></td>
-                <td ></td>
-                <td ></td>
-                <td ></td>
-                <td ></td>
-            </tr>
-            <tr class="tirashrashjdfg">               
-                <th>S2</th>
-                   <td></td>
-                   <td></td>
-                   <td></td>
-                   <td></td>
-                   <td></td>
-                   <td></td>
-                   <td></td>
-                   <td></td>
-                   <td></td>
-                   <td></td>
-                   <td></td>
-                   <td></td>
-                   <td></td>
-                   <td></td>
-                   <td></td>
-               </tr>
-
-               <tr class="tirashrashjdfg">               
-                <th>S3</th>
-                   <td></td>
-                   <td></td>
-                   <td></td>
-                   <td></td>
-                   <td></td>
-                   <td></td>
-                   <td></td>
-                   <td></td>
-                   <td></td>
-                   <td></td>
-                   <td></td>
-                   <td></td>
-                   <td></td>
-                   <td></td>
-                   <td></td>
-               </tr>
-               <tr class="tirashrashjdfg">               
-                <th>S4</th>
-                   <td></td>
-                   <td></td>
-                   <td></td>
-                   <td></td>
-                   <td></td>
-                   <td></td>
-                   <td></td>
-                   <td></td>
-                   <td></td>
-                   <td></td>
-                   <td></td>
-                   <td></td>
-                   <td></td>
-                   <td></td>
-                   <td></td>
-               </tr>
-            
-        </table>
+    <div class="">
+        <div class="tableima">
+            <table  class="table-bordered  text-center bg-white tableroomss">
+                <tr>
+                    <td colspan="2"></td>  
+                    <td >8-9</td>
+                    <td>9-10</td>
+                    <td>10-11</td>
+                    <td>11-12</td>
+                    <td>12-1</td>
+                    <td>1-2</td>
+                    <td>2-3</td>
+                    <td>3-4</td>
+                    <td>4-5</td>
+                    <td>5-6</td>
+                    <td>6-7</td>
+                    <td>7-8</td>
+                    <td>8-9</td>
+                    <td>9-10</td>
+                    <td>10-11</td>
+                </tr>
+                <tr style="background-color: orange; color: white;">
+                    <th rowspan="5" style="transform: rotate(-90deg); " >
+                        IMA
+                    </th>
+                </tr>
+                <tr class="tirashrashjdfg">               
+                 <th style="color: orange;">R1</th>
+                    <td ></td>
+                    <td ></td>
+                    <td ></td>
+                    <td ></td>
+                    <td ></td>
+                    <td >INFO</td>
+                    <td ></td>
+                    <td ></td>
+                    <td ></td>
+                    <td ></td>
+                    <td ></td>
+                    <td ></td>
+                    <td ></td>
+                    <td ></td>
+                    <td ></td>
+                </tr>
+                <tr class="tirashrashjdfg">               
+                    <th style="color: orange;">R2</th>
+                       <td></td>
+                       <td></td>
+                       <td></td>
+                       <td>ENG</td>
+                       <td></td>
+                       <td></td>
+                       <td></td>
+                       <td>AR</td>
+                       <td></td>
+                       <td></td>
+                       <td></td>
+                       <td></td>
+                       <td></td>
+                       <td></td>
+                       <td></td>
+                   </tr>
+    
+                   <tr class="tirashrashjdfg">               
+                    <th style="color: orange;">R3</th>
+                       <td></td>
+                       <td></td>
+                       <td></td>
+                       <td></td>
+                       <td>FR</td>
+                       <td></td>
+                       <td></td>
+                       <td></td>
+                       <td></td>
+                       <td></td>
+                       <td></td>
+                       <td></td>
+                       <td></td>
+                       <td></td>
+                       <td></td>
+                   </tr>
+                   <tr class="tirashrashjdfg">               
+                    <th style="color: orange;">R4</th>
+                       <td></td>
+                       <td></td>
+                       <td>ENG</td>
+                       <td></td>
+                       <td></td>
+                       <td></td>
+                       <td></td>
+                       <td></td>
+                       <td></td>
+                       <td></td>
+                       <td></td>
+                       <td></td>
+                       <td></td>
+                       <td></td>
+                       <td></td>
+                   </tr>
+                
+            </table>
+        </div>
     </div>
 @endsection
